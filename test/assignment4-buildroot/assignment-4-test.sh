8b2a300061a9bdc0b64878bd189fc9d0291e45ae
Dan Walkes <danwalkes@trellis-logic.com>
2020-08-01 18:07:50 -0600

    Cleanup test stage
    Don't depend on build outputs other than the image in the validate
    stage.  Remove checks on pre-image content and move all validation
    into the image itself.

--- test/assignment4-buildroot/assignment-4-test.sh
+++ test/assignment4-buildroot/assignment-4-test.sh
@@ -7,14 +7,9 @@ source assignment-timeout
 
 script_dir="$( cd "$(dirname "$0")" ; pwd -P )"
 testdir=$1
-qemu_executable_path=/bin	#Path where writer,finder,tester.sh are stored
-ROOTFS_PATH=buildroot/output/target/${qemu_executable_path}		# add ${script_dir} before buildroot to make it an absolute path 
-build_success_status=1		#1 indicates false
-build_again=1			#1 indicates false
 
 
 # Invoke docker script with --env SKIP_BUILD=1 --env DO_VALIDATE=1 to perform a validation only
-# test
 echo "starting test with SKIP_BUILD ${SKIP_BUILD} and DO_VALIDATE ${DO_VALIDATE}"
 if [[ -z ${SKIP_BUILD} || ${SKIP_BUILD} -eq 0 ]]; then
     . ./assignment-4-build.sh ${testdir}
@@ -23,13 +18,6 @@ fi
 
 if [[ -z ${DO_VALIDATE} || ${DO_VALIDATE} -eq 1 ]]; then
 	pushd ${testdir}
-	# save_build_binaries ${testdir}
-
-	# Validating Makefile Flags: Wall and Werror
-	validate_makefile_flags
-	echo "Test directory is ${testdir}"
-	# Validate executables: writer, finder.sh and tester.sh are in ${qemu_executable_path}
-	validate_executables "${qemu_executable_path}"	
 
 	# Running qemu instance in background
 	validate_qemu
