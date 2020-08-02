770dbe7e905b41d43d8770acd184f681668e5180
Dan Walkes <danwalkes@trellis-logic.com>
2020-08-01 20:23:33 -0600

    Remove duplicate test script

--- test/assignment4-buildroot/assignment-test.sh
+++ test/assignment4-buildroot/assignment-test.sh
@@ -10,7 +10,6 @@ source assignment-timeout
 script_dir="$(pwd -P )"
 testdir=$1
 qemu_executable_path=/bin	#Path where writer,finder,tester.sh are stored
-ROOTFS_PATH=buildroot/output/target/${qemu_executable_path}		# add ${script_dir} before buildroot to make it an absolute path 
 
 # Invoke docker script with --env SKIP_BUILD=1 --env DO_VALIDATE=1 to perform a validation only
 # test
@@ -26,19 +25,14 @@ fi
 if [[ -z ${DO_VALIDATE} || ${DO_VALIDATE} -eq 1 ]]; then
     echo "Starting validation step"
 	pushd ${testdir}
-	# save_build_binaries ${testdir}
-
-	# Validating Makefile Flags: Wall and Werror
-	validate_makefile_flags
-	echo "Test directory is ${testdir}"
-	# Validate executables: writer, finder.sh and tester.sh are in ${qemu_executable_path}
-	validate_executables "${qemu_executable_path}"	
 
 	# Running qemu instance in background
 	validate_qemu
 
 	# Validating test cases inside qemu
 	validate_assignment2_checks "${script_dir}" "${qemu_executable_path}"
+
+    add_validate_error "catchme"
 
 	echo "Killing qemu"
 	killall qemu-system-aarch64
