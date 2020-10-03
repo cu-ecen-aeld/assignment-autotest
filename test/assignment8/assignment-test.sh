49ac4ca1d3acac65a0589a722bc2c94ee14df424
Akshita Bhasin <akshita.bhasin@colorado.edu>
2020-10-03 15:45:05 -0600

    Updated instructions

--- test/assignment8/assignment-test.sh
+++ test/assignment8/assignment-test.sh
@@ -6,10 +6,11 @@ echo "STEPS TO MANUALLY TEST ASSIGNMENT 8"
 
 echo "After following steps 3 and 4,"
 echo "CD into your aesd-char-driver DIRECTORY and do a make."
+echo "RUN ./aesdchar_unload, followed by ./aesdchar_load"
 echo "From you main root assignment directory,"
 echo "RUN ./assignment-autotest/test/assignment8/drivertest.sh to verify you implementation"
 
-echo "After following step 5,"
-echo "From you main root assignment directory,"
+echo "After following step 5, unload and load your aesdchar device"
+echo "From you main root assignment directory"
 echo "RUN your aesdsocket"
 echo "And verify with it runs against sockettest.sh by running ./assignment-autotest/test/assignment8/sockettest.sh"
