07a926375f64acef427adb929eb865630694fa47
Charan <charan@Ubuntu.myguest.virtualbox.org>
2024-10-12 23:50:46 -0600

    assignment2: additional checks
    * Updated test script to fix automatic clean and build of writer
    which should have been done in the finder-test.sh
    * Updated debug information for failure cases

--- test/assignment2/assignment-test.sh
+++ test/assignment2/assignment-test.sh
@@ -8,27 +8,45 @@ SOURCE_DIR=$(realpath ${SCRIPTS_DIR}/../../../)
 pushd ${SOURCE_DIR}/finder-app
 
 make clean
-make
-
-./writer
-rc=$?
-if [ $rc -ne 1 ]; then
-	add_validate_error "writer should have exited with return value 1 if no parameters were specified"
+if [ -x "./writer" ]; then
+    echo "ERROR: make clean does not clean up the writer executable in ${SOURCE_DIR}/finder-app"
+    exit 1
 fi
 
-./writer "$filedir"
-rc=$?
-if [ $rc -ne 1 ]; then
-	add_validate_error "writer should have exited with return value 1 if write string is not specified"
+make
+if [ -x "./writer" ]; then
+    ./writer
+    rc=$?
+    if [ $rc -ne 1 ]; then
+        add_validate_error "writer should have exited with return value 1 if no parameters were specified"
+    fi
+
+    ./writer "$filedir"
+    rc=$?
+    if [ $rc -ne 1 ]; then
+        add_validate_error "writer should have exited with return value 1 if write string is not specified"
+    fi
+    echo "Performing make clean"
+    make clean
+else
+    echo "Makefile Error, Failed to generate writer executable in ${SOURCE_DIR}/finder-app."
+    exit 1
 fi
 
 ./finder-test.sh
 rc=$?
+# Check if writer executable exists after finder-test.sh
+if [ ! -x "./writer" ]; then
+    echo "ERROR: ./writer executable does not exist after executing finder-test.sh in ${SOURCE_DIR}/finder-app. Make sure finder-test.sh includes the necessary make step."
+    exit 1
+fi
+
 if [ $rc -ne 0 ]; then
-	add_validate_error "finder-test.sh execution failed with return code $rc"
+    add_validate_error "finder-test.sh execution failed with return code $rc"
 fi
 
 if [ ! -z "${validate_error}" ]; then
     echo "Validation failed with error list ${validate_error}"
     exit 1
 fi
+
